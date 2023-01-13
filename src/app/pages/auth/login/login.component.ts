9d813cdd1668e06996ec6c86ff880375dfd8dcb3
Josue Ale <jam9814@hotmail.com>
2023-01-13 16:48:53 -0500

    migrated from template form to reactive form @ login

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,22 +1,32 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 import { AuthService } from 'src/app/services/auth.service';
-import { LoginForm } from 'src/app/types/auth.interface';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
-  form: LoginForm = {
-    email: '',
-    password: '',
-  };
+  constructor(private authSvc: AuthService, private fb: FormBuilder) {}
 
-  constructor(private authSvc: AuthService) {}
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  isValidField(field: string) {
+    return (
+      !this.loginForm.controls[field].valid &&
+      this.loginForm.controls[field].touched
+    );
+  }
 
   handleOnSubmit({ value: form }: NgForm) {
+    // TODO validate answer
+    // TODO add toast (even if its boring AF)
+    // ?  redirect to login???
+
     this.authSvc.login(form).subscribe();
   }
 }
