e94efb2cfc5612330d2de2a264f516b07993ef7b
Josue Ale <jam9814@hotmail.com>
2023-01-13 17:03:10 -0500

    updated handleOnSubmit funciton parameter type, login not submitting fixed

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -22,7 +22,7 @@ export class LoginComponent {
     );
   }
 
-  handleOnSubmit({ value: form }: NgForm) {
+  handleOnSubmit({ value: form }: FormGroup) {
     // TODO validate answer
     // TODO add toast (even if its boring AF)
     // ?  redirect to login???
